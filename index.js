76439d6648882d60ae9a406ff1ded55de114ede1
Santiago <santiago.canti.gcba@gmail.com>
2017-05-17 14:41:14 -0300

    first group of calls implemented

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hello/:name', (req, res) => {
-  res.send({message: `hello ${req.params.name}`});
+app.get('/api/product', (req, res) => {
+  res.status(200).send({products: []});
 })
+
+app.get('/api/product/:productId', (req, res) => {
+
+})
+
+app.post('/api/product', (req, res) => {
+  console.log(req.body);
+  res.status(200).send({message: 'product loaded'});
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/productId', (req, res) => {
+
+})
+
 app.listen(port, () => {
   console.log(`API REST listening in http://localhost:${port}`);
 })
